5d548844152b63810f9c9364b2b92f8483d995e9
Gie Spaepen <giespaepen@outlook.com>
2020-10-27 13:12:31 +0100

    #213 Configured all questions

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,9 +1,10 @@
 import { builder } from "./builder";
-import { Question, QuestionId } from "./domain";
+import { IQuestion, QuestionId, Questions } from "./domain";
 
 export const INITIAL_QUESTION_ID: QuestionId = "0";
 
-export const questions: Array<Question> = [
+export const questions: Questions= [
+    // Introduction and main question
     builder(INITIAL_QUESTION_ID).withTitle("Start de Covid Calculator.").withNext("1"),
     builder("1").withTitle("Is er een hoog risico conrtact geweest met een bevestigd covid geval?").withYesNo("r1", "s1"),
 
@@ -11,5 +12,31 @@ export const questions: Array<Question> = [
     builder("r1").withTitle("Heeft de patient in de afgelopen 2 maanden een positieve test (PCR) gehad?").withYesNo("f1", "r2"),
     builder("f1").withTitle("Geen quarantaine nodig").final(),
     builder("r2").withTitle("Is het een huisgenoot?").withYesNo("r2a", "r2b"),
-    builder("r2a").withTitle("Sinds wanneer is de huisgenoot symptomatisch?").withDaysAgo("r3")
+    builder("r2a").withTitle("Sinds wanneer is de huisgenoot symptomatisch?").withDaysAgo("r3"),
+    builder("r2b").withTitle("Wanneer is het laatste risicocontact geweest?").withDaysAgo("r4"),
+    builder("r3").withTitle("Is het mogelijk binnenshuis quarantaine te creëeren?").withYesNo("r4", "f2"),
+    builder("r4").withTitle("Vanaf welke datum?").withDaysAgo("f2"),
+    builder("f2").withTitle("Aantal dagen quarantaine").final(),
+
+    // S-side of the flow chart
+    builder("s1").withTitle("Wanneer zijn de klachten begonnen?").withNext("s2"),
+    builder("s2").withTitle("Is er een test gedaan?").withYesNo("s3a", "s3b"),
+    builder("s3a").withTitle("Wanneer").withDaysAgo("s4"),
+    builder("s3b").withTitle("Test indien van toepassing").withNext("f5"),
+    builder("s4").withTitle("Is de uitkomst reeds gekend?").withYesNo("s5", "f5"),
+    builder("s5").withTitle("Was de test positief?").withYesNo("f3", "s6"),
+    builder("s6").withTitle("Heeft men een ook een hoog risico contact gehad?").withYesNo("r2", "f4"),
+    builder("f4").withTitle("Quarantaine beëindigd").final()
 ];
+
+export function isQuestionId(id: string): id is QuestionId {
+    return !!questions.map(q => q.id).find(i => i === id);
+}
+
+export function findQuestion(id: string): IQuestion | undefined {
+    return questions.find(q => q.id === id);
+}
+
+export const findInitialQuestion = (): IQuestion | undefined  => {
+    return findQuestion(INITIAL_QUESTION_ID);
+};
