aea6ec80afceb4582153ce1902008c9952944df7
Gie Spaepen <giespaepen@outlook.com>
2020-11-09 23:23:12 +0100

    #33 Improved start screen

--- src/components/Questions/Start.tsx
+++ src/components/Questions/Start.tsx
@@ -11,13 +11,9 @@ export const Start: React.FC<QuestionProps<INextQuestion>> = (props) => {
         <Next {...props}>
             <div className="question__help help">
                 <div className="help__content">
-                    <strong>Een woordje uitleg...</strong> Gebruik de onderstaande knoppen
+                    Gebruik de onderstaande knoppen
                     om de vragen te beantwoorden. Klik op <SyncIcon verticalAlign="middle" />
-                    om opnieuw te beginnen. <br/>
-
-                    Sommige vragen kan je gewoon op 'Volgende' klikken, de meeste vragen zijn Ja-Nee vragen. 
-                    In een laatste type kan je aangeven hoeveel dagen iets geleden is. Klik op het cijfer om
-                    het aantal dagen op te geven of op +/-.
+                    om opnieuw te beginnen.
                 </div>
             </div>
         </Next>
